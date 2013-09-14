abffee8b6c6c62fba86b17422bd5a74948c00f77
Liesl Wigand <wolfdrake15@gmail.com>
2013-09-13 19:33:06 -0700

    added assets from previous page. It is displaying some things correctly, but likely overwriting some css pieces that I am using. Images are all broken.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,18 @@ describe "StaticPages" do
     	visit '/static_pages/home'
     	expect(page).to have_content('Liesl Wigand')
     end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Liesl Wigand: Computer Scientist, Graduate Student | Home')
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Liesl Wigand: In Progress')
+    end
   end
 
   describe "Help page" do
@@ -13,12 +25,36 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('About')
   	end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | About'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Liesl Wigand: Computer Scientist, Graduate Student | About')
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Liesl Wigand: In Progress')
+    end
   end
 
   describe "Fun Page" do
     it "should have the content 'FUN STUFF'" do
       visit '/static_pages/fun'
       expect(page).to have_content('FUN STUFF')
+    end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | Fun'" do
+      visit '/static_pages/fun'
+      expect(page).to have_title('Liesl Wigand: Computer Scientist, Graduate Student | Fun')
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/fun'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/fun'
+      expect(page).to have_content('Liesl Wigand: In Progress')
     end
   end
 end
