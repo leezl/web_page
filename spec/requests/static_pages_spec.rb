7825314d5e95cad81fd9e25ee48fe9464da05114
Liesl Wigand <wolfdrake15@gmail.com>
2013-09-15 16:09:52 -0700

    Fixed up a bunch of broken css. Need to add routes to other pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -59,4 +59,62 @@ describe "StaticPages" do
       expect(page).to have_content('Liesl Wigand: In Progress')
     end
   end
+
+  describe "Fun Book Page" do
+    it "should have the content 'Fun: Books'" do
+      visit '/static_pages/subFunBooks'
+      expect(page).to have_content('Fun: Books')
+    end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | Fun'" do
+      visit '/static_pages/subFunBooks'
+      expect(page).to have_title("#{base_title} | Fun Books")
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/subFunBooks'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/subFunBooks'
+      expect(page).to have_content('Liesl Wigand: In Progress')
+    end
+  end
+
+  describe "Fun Comics Page" do
+    it "should have the content 'Fun: Comics'" do
+      visit '/static_pages/subFunComics'
+      expect(page).to have_content('Fun: Comics')
+    end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | Fun'" do
+      visit '/static_pages/subFunComics'
+      expect(page).to have_title("#{base_title} | Fun Comics")
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/subFunComics'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/subFunComics'
+      expect(page).to have_content('Liesl Wigand: In Progress')
+    end
+  end
+
+  describe "Fun Game Page" do
+    it "should have the content 'Fun: Games'" do
+      visit '/static_pages/subFunGames'
+      expect(page).to have_content('Fun: Games')
+    end
+    it "should have the title 'Liesl Wigand: Computer Scientist, Graduate Student | Fun'" do
+      visit '/static_pages/subFunGames'
+      expect(page).to have_title("#{base_title} | Fun Games")
+    end
+    it "should have a footer containing 'Hosted by:'" do
+      visit '/static_pages/subFunGames'
+      expect(page).to have_content('Hosted by:')
+    end
+    it "should have a header containing 'Liesl Wigand: In Progress'" do
+      visit '/static_pages/subFunGames'
+      expect(page).to have_content('Liesl Wigand: In Progress')
+    end
+  end
+
 end
